f287d6322a58c17fec018dc0d31b88c48b452748
Alexander Jeuk <alexander2.jeuk@t-online.de>
2018-12-11 13:30:04 +0100

    Update main.h

--- WMIC_static_spoofer/main.h
+++ WMIC_static_spoofer/main.h
@@ -6,7 +6,7 @@
 #include <Ntstrsafe.h>
 
 //Change that (Offsets are for Windows 10 16299.125)
-#define	POOLTAG01 'ahIK'
+#define POOLTAG01 'ahIK'
 #define RegDevIntOFF 0x12814 //RaidUnitRegisterInterfaces in storport.sys SIG: 48 89 5C 24 ? 55 56 57 48 83 EC 50
 #define MAX_HDDS 10
 #define SERIAL_MAX_LENGTH 15
